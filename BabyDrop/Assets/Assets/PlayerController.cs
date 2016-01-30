373c152ba6bac95e60f17c06de96a9b7a041bca1
unknown <ejcerini@FL222-24.admin.wpi.edu>
2016-01-30 00:22:23 -0500

    I did a thing.

---
+++ BabyDrop/Assets/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public int playerNum;
+
+    private string playerAxis;
+
+    [SerializeField]
+    float speed = 5.0f;
+
+    private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+        playerAxis = "P" + playerNum + "_Horizontal";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis(playerAxis);
+        moveDirection = new Vector3(horizontal, 0, 0);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        transform.position += moveDirection * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+}
