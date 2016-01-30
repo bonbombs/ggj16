388e2ce09a7693fd431ce1f3a515e013f69d5f72
bonbombs <kelly.zhang2306@gmail.com>
2016-01-30 14:33:36 -0500

    added crummy backdrop. player sprites move and animate and turn around when not holding blanket

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
 
-    public int playerNum;
+    [SerializeField]
+    private int playerNum;
+
+    public bool isHoldingBlanket = false;
 
     private string playerAxis;
-
+    private string playerDropBlanketKey;
+    private Animator animator;
+    private SpriteRenderer sr;
+    private Text label;
+    private Vector3 labelOffset = new Vector3(0, 1.5f);
     [SerializeField]
     float speed = 5.0f;
 
@@ -15,15 +24,35 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         playerAxis = "P" + playerNum + "_Horizontal";
+        playerDropBlanketKey = "P" + playerNum + "_Drop_Blanket";
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        Text[] labels = FindObjectsOfType<Text>();
+        foreach (Text l in labels){
+            if (l.name.Contains(playerNum.ToString())) label = l;
+        }
+        label.transform.position = transform.position + labelOffset;
     }
 	
 	// Update is called once per frame
 	void Update () {
         float horizontal = Input.GetAxis(playerAxis);
+        float drop = Input.GetAxis(playerDropBlanketKey);
+        if (horizontal != 0) animator.SetBool("isMoving", true);
+        else animator.SetBool("isMoving", false);
         moveDirection = new Vector3(horizontal, 0, 0);
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= speed;
         transform.position += moveDirection * Time.deltaTime;
+        label.transform.position = transform.position + labelOffset;
+
+        if (!isHoldingBlanket)
+        {
+            if (horizontal > 0) sr.flipX = false;
+            else if (horizontal < 0) sr.flipX = true;
+        }
+
+        if (drop != 0) isHoldingBlanket = !isHoldingBlanket;
 
         if (Input.GetKey(KeyCode.Escape))
         {
---
+++ Assets/PlayerTurnController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTurnController : MonoBehaviour {
+
+    public GameObject player1, player2;
+    private PlayerController p1c, p2c;
+    private SpriteRenderer p1sr, p2sr;
+
+	// Use this for initialization
+	void Start () {
+        p1c = player1.GetComponent<PlayerController>();
+        p2c = player2.GetComponent<PlayerController>();
+        p1sr = player1.GetComponent<SpriteRenderer>();
+        p2sr = player2.GetComponent<SpriteRenderer>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(player1.transform.position.x > player2.transform.position.x)
+        {
+        }
+	}
+}
